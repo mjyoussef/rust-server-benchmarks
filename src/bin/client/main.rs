ba593754112cf19abfaca381fba03459af79c459
mjyoussef <myoussef3927@gmail.com>
2025-12-17 23:07:46 -0500

    feat: save results

--- src/bin/client/main.rs
+++ src/bin/client/main.rs
@@ -3,11 +3,12 @@ mod open_loop;
 
 use std::{
     net::{Ipv4Addr, SocketAddrV4},
+    path::PathBuf,
     time::Duration,
 };
 
 use clap::{Parser, ValueEnum};
-use rust_server_benchmarks::protocol::Work;
+use rust_server_benchmarks::{protocol::Work, write_stats};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -38,6 +39,10 @@ struct Args {
     #[arg(long, default_value_t = 1)]
     num_clients: u16,
 
+    /// Directory to write results to
+    #[arg(short, long)]
+    dir: PathBuf,
+
     /// The workload type.
     #[command(subcommand)]
     work: Work,
@@ -54,6 +59,7 @@ fn main() {
     let addr = SocketAddrV4::new(args.ip, args.port);
     let runtime = Duration::from_secs(args.runtime);
     let delay = Duration::from_micros(args.delay);
+    let dir = args.dir;
 
     match args.kind {
         Kind::Closed => {
@@ -63,7 +69,11 @@ fn main() {
                 work: args.work,
                 num_clients: 1,
             };
-            cfg.run();
+            let lrs = cfg.run();
+            let n_reqs = lrs.len();
+            let path = dir.join("closed/stats.txt");
+            println!("{:?}", path);
+            write_stats(lrs, n_reqs, None, args.runtime, &path).unwrap();
         }
         Kind::Open => {
             let cfg = open_loop::Config {
@@ -72,7 +82,9 @@ fn main() {
                 delay,
                 work: args.work,
             };
-            cfg.run();
+            let (n_reqs, lrs) = cfg.run();
+            let path = dir.join("open/stats.txt");
+            write_stats(lrs, n_reqs, Some(args.delay), args.runtime, &path).unwrap();
         }
-    }
+    };
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,61 @@
 pub mod protocol;
 
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::{
+    fs::{self, File},
+    io::{Result, Write},
+    path::PathBuf,
+    time::{SystemTime, UNIX_EPOCH},
+};
 
+use crate::protocol::LatencyRecord;
+
+/// Gets the current time (in nanoseconds) since the UNIX epoch.
 pub fn get_time() -> u64 {
     SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .unwrap()
         .as_nanos() as u64
+}
+
+/// Saves performance statistics.
+///
+/// # Arguments
+///
+/// * `lrs` - The latency records.
+/// * `n` - Number of requests sent (this should match `lrs.len()` for a closed
+///    loop request generator).
+/// * `delay` - The delay in microseconds (this should be `None` for a closed
+///    loop request generator).
+/// * `runtime` - Total runtime in microseconds.
+/// * `path` - The destination file path.
+pub fn write_stats(
+    lrs: Vec<LatencyRecord>,
+    n: usize,
+    delay: Option<u64>,
+    runtime: u64,
+    path: &PathBuf,
+) -> Result<()> {
+    // Calculate the 50, 95, and 99th percentile latencies
+    let mut latencies: Vec<_> = lrs.iter().map(|lr| lr.recv_time - lr.send_time).collect();
+
+    latencies.sort();
+    let p_50 = latencies[latencies.len() / 2] as f64 / 1000.0;
+    let p_95 = latencies[(latencies.len() as f64 * 0.95 as f64) as usize] as f64 / 1000.0;
+    let p_99 = latencies[(latencies.len() as f64 * 0.99 as f64) as usize] as f64 / 1000.0;
+
+    // Calculate the attempted, offered, and achieved throughput
+    let attempted = match delay {
+        Some(delay) => (1000000.0 / (delay as f64)).to_string(),
+        None => String::from("IGNORE"),
+    };
+    let offered = n as u64 / runtime;
+    let achieved = latencies.len() as u64 / runtime;
+
+    fs::create_dir_all(path.parent().expect("file path is missing directory"))?;
+    let mut file = File::create(path).unwrap();
+
+    writeln!(file, "{p_50}, {p_95}, {p_99}")?;
+    writeln!(file, "{attempted}, {offered}, {achieved}")?;
+
+    Ok(())
 }
